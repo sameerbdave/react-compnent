1b2051944e73418c60fbaf7c3b66370bb6c48010
Sameer Dave <sameerbdave@gmail.com>
2022-10-10 18:13:32 +0530

    segregation-components

--- example/src/App.js
+++ example/src/App.js
@@ -4,18 +4,26 @@ import Accordin from './components/Accordin';
 import FormMobileOtp from './components/FormMobileOtp';
 import OfferCarosel from './components/OfferCarosel';
 import Tabs from './components/Tabs';
+import Offers from './components/Offers';
 
 function App() {
   return (
     <>
+      <h1 className="text-center mt-5">Carouse Component</h1>
       <OfferCarosel />
+      <h1 className="text-center mt-5">Form components</h1>
+      <hr/>
       <FormMobileOtp/>
       {/* <Accordian /> */}
+      <h1 className="text-center mt-5">Tabs components</h1>
       <hr/>
       <Tabs />
+      <h1 className="text-center mt-5">Tabs Accordian</h1>
       <hr/>
       <Accordin/>
-      
+      <h1 className="text-center mt-5">Card  components</h1>
+      <hr/>
+      <Offers/>
     </>
  
   );
--- example/src/components/Tabs.js
+++ example/src/components/Tabs.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react'
 
-const faqs = [
+const tabsData = [
     {
         id: 1,
-        header: "What is Lorem Ipsum?",
+        header: "Tabs 1",
         text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`
     },
     {
         id: 2,
-        header: "Where does it come from?",
+        header: "Tabs 2",
         text: `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. `
     },
     {
         id: 3,
-        header: "Why do we use it?",
+        header: "Tabs 3",
         text: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classNameical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature,`
     },
     {
         id: 4,
-        header: "Where can I get some?",
+        header: "Tabs 5",
         text: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.`
     }
 ]
@@ -36,19 +36,8 @@ function Tabs() {
     }
     return (
         <div className='wrapper'>
-            {/* {faqs.map((item) => (
-                <>
-                    <ul className='nav nav-tabs'>
-                        <li className='nav-item'><a className='nav-link'>{item.header}</a></li>
-                    </ul>
-                    <div className='tab-content'>
-                        <div className='tab-pane'>{item.text}</div>
-                    </div>
-                </>
-            ))} */}
-
             <ul className="nav nav-tabs">
-            {faqs.map((item,i) => (
+            {tabsData.map((item,i) => (
                 <li className="nav-item">
                     <a className={actv === i ? "nav-link active" : "nav-link"} onClick={()=> toggle(i)}>{item.header}</a>
                 </li>
@@ -56,7 +45,7 @@ function Tabs() {
             </ul>
 
             <div className="tab-content">
-            {faqs.map((item,i) => (
+            {tabsData.map((item,i) => (
                 <div className={actv === i ? "tab-pane active" : "tab-pane"}>{item.text}</div>
                 ))}
             </div>
